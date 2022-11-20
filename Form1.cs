ad56f9bb8cd6d7663e5c9435655017f3d1b7d1df
Nikolay Belykh <nbelyh@gmail.com>
2022-11-20 14:40:43 +0100

    Add project files.

---
+++ Form1.cs
@@ -0,0 +1,27 @@
+﻿using System.Windows.Forms;
+using Visio = Microsoft.Office.Interop.Visio;
+
+namespace DragDropAddin
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void listView1_ItemDrag(object sender, ItemDragEventArgs e)
+        {
+            var item = (ListViewItem)e.Item;
+
+            var app = Globals.ThisAddIn.Application;
+
+            var myStencil = app.Documents.OpenEx("BASIC_M.VSS",
+                (short)Visio.VisOpenSaveArgs.visOpenDocked | (short)Visio.VisOpenSaveArgs.visOpenRO);
+
+            var masterToDrag = myStencil.Masters[item.Text];
+            var data = new DataObject(masterToDrag);
+            DoDragDrop(data, DragDropEffects.Copy);
+        }
+    }
+}
---
+++ ThisAddIn.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Office.Interop.Visio;
+
+namespace DragDropAddin
+{
+    public partial class ThisAddIn
+    {
+        Form1 _form1 = new Form1();
+
+        private void ThisAddIn_Startup(object sender, System.EventArgs e)
+        {
+            Application.Documents.Add("");
+            _form1.Show();
+        }
+
+        private void ThisAddIn_Shutdown(object sender, System.EventArgs e)
+        {
+            _form1.Close();
+        }
+
+        #region VSTO generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InternalStartup()
+        {
+            this.Startup += new System.EventHandler(ThisAddIn_Startup);
+            this.Shutdown += new System.EventHandler(ThisAddIn_Shutdown);
+        }
+        
+        #endregion
+    }
+}
